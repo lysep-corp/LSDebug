87ca79353054d86407fe35314fcec3a3d590ea8c
septillioner <septillioner@protonmail.com>
2020-04-18 01:59:00 +0300

    Variable Debugger Complete

--- LSDebug/LSDebug.cs
+++ LSDebug/LSDebug.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Data;
+using System.Windows.Media.Imaging;
+using System.IO;
+
 namespace LSDebug
 {
     public enum TextType
@@ -22,6 +25,7 @@ namespace LSDebug
         Percent = 0,
         Pixel = 1
     }
+    #region LS Splitter Panel
     class LSSplitterPanel : Panel
     {
         public Panel Panel1, Panel2;
@@ -115,10 +119,12 @@ namespace LSDebug
        
 
     }
+    #endregion
+    #region LSDebugUI Main
     class LSDebugUI : Form{
         private LSDebug LST = new LSDebug();
-        private LSDebugVariable LSTV = new LSDebugVariable();
-        private LSSplitterPanel SPC = new LSSplitterPanel();
+        public LSDebugVariable LSTV = new LSDebugVariable();
+        public LSSplitterPanel SPC = new LSSplitterPanel();
         public LSDebugUI() : base()
         {
             SPC.BackColor = Color.FromArgb(23, 23, 23);
@@ -128,8 +134,8 @@ namespace LSDebug
             this.Controls.Add(SPC);
             SPC.Panel1.Controls.Add(LSTV);
             SPC.Panel2.Controls.Add(LST);
-            SPC.WidthPanel= 290;
-            this.Size = new Size(870, 460);
+            SPC.WidthPanel= 380;
+            this.Size = new Size(980, 460);
 
             LST.Visible = true;
             LST.Dock = DockStyle.Fill;
@@ -161,9 +167,9 @@ namespace LSDebug
             }
         }
         #region VariableDebugger
-        public async void AddVariable(string VariableName,int value)
+        public async void SetVariable(string VariableName,int value)
         {
-            LSTV.AddVariable(VariableName, value);
+            LSTV.SetVariable(VariableName, value);
         }
         #endregion
         #region ConsoleDebugger
@@ -209,6 +215,7 @@ namespace LSDebug
         }
         #endregion
     }
+    #endregion
     #region Build Of ConsoleDebugger
     class LSDebug : RichTextBox
     {
@@ -556,6 +563,8 @@ namespace LSDebug
         #endregion
     }
     #endregion
+    #region Base64Texture
+    #endregion
     #region Build Of VariableDebugger
     class LSDebugVariable : DataGridView
     {
@@ -567,6 +576,19 @@ namespace LSDebug
         public static Color CellStyle_SelectionColor = Color.FromArgb(29, 29, 29);
         public static Color CellStyleHeader_BackgroundColor = Color.FromArgb(25, 25, 25);
         public static Color CellStyleHeader_SelectionColor = Color.FromArgb(29, 29, 29);
+        public static Bitmap VariableIcon = Base64StringToBitmap("iVBORw0KGgoAAAANSUhEUgAAADoAAABACAYAAABLAmSPAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAcdAAAHHQFqqUP2AAAAB3RJTUUH5AQRETceK8gxHgAACMdJREFUaN7t29tvI9UdwPHvmRnb8T2J41tsx8nSpWzZQmm5FAGlFKh6E60QRQgKC2rDU6X+C/0Hqj6XSlzaQqEC9aEPldrCIrVCrSq1C2VZWLabbJzEju0kvk98m9OH8QSv18lmN7azbPc8WfaMz+8zv3OZyxnBEMsxjm9/lsigRD6ioPxkzKP5PRP2X0wl3M///p2TS187/BmO3BXg2IuzQ4tFjAj4HQXlGa/ffft0IuSQQGYl22406+/ZneoLvqmx1/9+KpU+Gg4zEXXy0xM3X9nQHmBYIr+nCOVpn99zaywZ1qKJIC73GAC16harS1lWU1lD1/V3VZt43umxvbGULqSDHjcun52fr95xZUEtoEQCTEvkw6pQj/nG3bfEkhE1mgjidDnMbWSn4k7NerXOairLytKaUa3UTiDk83aH+sZmeSvjttnRbArP1e45WGgPMCGRj6hCfco/4bkpPhtRIvEgY077ecALArDAtTrpVI6Vc2tGuVQ5YRjyBVUVb9Rb7bRNKAgheMH46mihPcBZiXxUVdQnxye9N8ZnIyISm8IxZjd/lXv7Twu8pTdIL+dYXswYpULlXcMwXhSI1yVyVXTCfYn7hgu1gAYGAnGdRD6mKdoT4wHvkcRclNB0AIfDtmP29hqR6IAzK3mWFzNGcbP8XttovyQQrwPLlwPeE9QC6mKLMem4XiIf11Tt8YmA73BiLkooGsDu0PYH3AFc32qSWcmxvJCRhc3y+22j/SuB+B2wdCngXaEWUEOlSeuIRP7ApmqPTQT9hxJzUUKRSWz2AQP7BSmgXm+ytpIntZCRxY3SBy2j/WuBeA1Y3Au4L9QCCoQwMI5K5JM2zfZoIOhPJuaiTEUmsdnUoQP7gRv1Jmur66QWMhTWS6daRutlgfithLPKLuBtaPccKBBKG+NmkE/ZbbZHAqGJeGIuwlRoAu0AgH3BjRbZ1XVSC2k210unW+3WywLxioFxRkW9ACy6gYBqIG8B+bTdZn94KjwRnTkUZTI4jqYpBw7sB242WmTTG6QW0mzki2da7dYrAvHKlqh/5JRj22ALqhnIW0E+47DbvxuMTIYTc1Emg35U9coD9gU3W+TSG6QWMmzkC2ebrdarAvEbB/ZTDZqIYxy/30A+7rDbHwpFJ6cSh6JMBHyfCmB/cJt8xszwera42Gg3X1NRXhVPK2+XDt8e9F5/UwxF92HUxV7n+Cu2CKDValPWN0ktrZA6VVjTVFX1ziTjxG/woTqgnIZSCpq1rr0+LUWa8dp9EEyoXBeYwv9vG+mP3wtrAqjlYOWf4JwE/yzEolDOdMDVTwG4A3SMgz8BDh/U1mH1X1BYFEgp0CyENKCShVoenAEYT0LsNqisQXHpCgV3gGPj4JsBhweqOdj8r9kihfgkXq17PyHMq4xqDvR1GJvsAmehtASNyhUAtoATZgZtbqhmYeNjaOqd0LqQF0B7wbU86BvgHDeb9PSt5kEoLkGjPHqwlCAUcE6ALwGaE6prkD8NrS0zbrFDPNpuI6wQ5tHTN0DfNJvIeBKiXzIPQvFcJ8NyuGApQVHMMcSXAM0BlQxUPoRWfXfgNnRPNVnXipuQKcCYH/wdsJ43M1wvDx5sAV0B8MVBsUElbXajdmNvwEuD9oILsFU0Rzd/EiJfNPt0cQnqpf2DpQRFBdck+GIgVBNYzUG7eWnAy4P2gOtFyP4HHF7wz0DkC2bWC+cuD2wB3QHwxsx9K6tQzYPRujzg/qC94BJkT4Ld0wUumH14q3hxsJSgaB3gtLl9ecWcC432/oCDgfaAG2XIdcC+GQjfBFulDrhwIVhKUDVwTYEnCrJtnqTom4MDDhbaC65A/gMous15LvR5aJSgsGQ2bSlBtYErCJ4wGE2zf+ub5onLIIHDgfaAm1XIfwi2lDlqho6aYL1gzoXtBhQWO83bMPcbNHC40D7g9Y+gmILJQ2Y/zJ3sGbCGfOIxXGgfcGkV3C0zi2IEQKsoo6nmE7BQRpLAA4bSdcJ9tUMP6qrnGvQqc17L6DXovp3/L1Aro6O+d3xtMLrapNf66PCkXDsFvKqgQhyMdfQZPaByMIPRQfXRkdY7YqRVnSKlRNfr5pcjCGJU08v2GkO9jpQSrd1q//GDE2fu3swXvYm5KF6/e/tp2nAiGD5QSigVKqQWMqSXc+V2q/03Dfh+rabfc/Z06tnMcv7BaCLoScxF8PiGAx5WRvsAK3pt608S+UsF5a/d64xcBsZ9AjHvcjsfmE6E3PHZCB6fa2BgKSFwGFS7+QhjEGgrtnKxSmohbQH/3AG+DejQaUg9i6pcBsb9AjHvdjvvj86EXInZCG7v/sFSQuB68y79fqG9wMxyrlKrbf1FIp/rBkJnQVX3zj1gt4HxgEDMuz3O+6YTYVd8NrwvsJQw9VnzOWf2/cuDdgOXFzOkU1kLaGXQWk9z/hK5fn/WA/YYGA8KxLNuj+ve2EzIGZuN4PY6EVwaWEqYusF8cnap0B2Ab3ZlsC9wV+gOYK+B8XWBmPd4XfdOz4TH4skwbq9zG7EXaPCI+WB3r9BtYKnK8kKGdCpXrdV0K4PHLwbcE3QX8DcEyrzH6/pKLBlyxJJh3J6Lg6WE4OfMu/UXg1rASqlqjaLVWlV/swN8a6/AS4LuAPYZGN9UUOY9PtfdsZmwI5YM4/KM7QiWEkI3mrXuBB008LKg/cAS/BLjWx3wXbFk2B5Lhrffb+kG7wY9D7jYaaJV/a0uoLWka/gvD1wEPC4xvq2gzHv87jvjybB9eiZ0HlhKCB81b4xZ0G7g8mKG1VSu1gE+NwjgQKD9wXKi84rWvNfv/nJ8NmKbngnhdDnMjB41j0r2pLl9pVTrALO1rgy+OSjgQKE7gCcl8iEF5Ufecfcd8WREm06EmL3TgdGGs+/UWD6XIb2UrVWr+vFOBgcOHAp0B3BAIh9ShPJDn99z25G7Qva63uKjf2Qrtar+9rAyOBLoDuCwRD6hoPxYIjcl8mcKyh8Aa1XhUIBW+R+ywUpGstRBiAAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMC0wNC0xN1QxNzo1NTozMC0wNDowMD8dq6cAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjAtMDQtMTdUMTc6NTU6MzAtMDQ6MDBOQBMbAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAABR0RVh0VGl0bGUAYmlnIGNvbG9yIGN1YmWxFoP/AAAAAElFTkSuQmCC"); 
+        public static Bitmap Base64StringToBitmap(string base64String)
+        {
+            Bitmap bmpReturn = null;
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+            memoryStream.Position = 0;
+            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
+            memoryStream.Close();
+            memoryStream = null;
+            byteBuffer = null;
+            return bmpReturn;
+        }
         public LSDebugVariable() : base()
         {
             this.BackgroundColor = MainThemeColor;
@@ -588,10 +610,12 @@ namespace LSDebug
             this.RowHeadersDefaultCellStyle.ForeColor = TextColor;
             this.RowHeadersDefaultCellStyle.Padding = new Padding(3, 2, 3, 2);
             this.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            this.RowHeadersWidth = 30;
             this.BorderStyle = BorderStyle.None;
             this.ColumnHeadersBorderStyle= DataGridViewHeaderBorderStyle.None;
             this.ReadOnly = true;
             this.EnableHeadersVisualStyles = false;
+            this.RowPostPaint += RowPostPaintEvent;
             DataGridViewCell cellTemplate = new DataGridViewTextBoxCell();
 
             DataGridViewColumn nameColumn = new DataGridViewColumn();
@@ -602,6 +626,10 @@ namespace LSDebug
             valueColumn.Name = "Value";
             valueColumn.HeaderText = "Value";
             valueColumn.CellTemplate = cellTemplate;
+            DataGridViewColumn hexColumn = new DataGridViewColumn();
+            hexColumn.Name = "Hex";
+            hexColumn.HeaderText = "Hex";
+            hexColumn.CellTemplate = cellTemplate;
             DataGridViewColumn typeColumn = new DataGridViewColumn();
             typeColumn.Name = "Type";
             typeColumn.HeaderText = "Type";
@@ -609,16 +637,87 @@ namespace LSDebug
             typeColumn.Width = 50;
             this.Columns.Add(nameColumn);
             this.Columns.Add(valueColumn);
+            this.Columns.Add(hexColumn);
             this.Columns.Add(typeColumn);
         }
-        public void AddVariable(string VariableName,int value)
+        public void RowPostPaintEvent(object sender,DataGridViewRowPostPaintEventArgs e)
         {
-            DataGridViewRow row = (DataGridViewRow)this.Rows[0].Clone();
-            row.Tag = VariableName;
-            row.Cells[0].Value = VariableName;
-            row.Cells[1].Value = String.Format("0x{0}", value.ToString("X8"));
-            row.Cells[2].Value = "int";
-            this.Rows.Add(row);
+
+            if (this.Rows[e.RowIndex].Cells[0].Value != null) {
+                Icon VariableIco = Icon.FromHandle(VariableIcon.GetHicon());
+                Graphics graphics = e.Graphics;
+
+                int w = 20;
+                int h = 20;
+                int x = e.RowBounds.X + ((this.RowHeadersWidth-w) / 2);
+                int y = e.RowBounds.Y + (this.Rows[e.RowIndex].Height - h) / 2;
+
+                Rectangle rectangle = new Rectangle(x, y, w, h);
+                graphics.DrawIcon(VariableIco, rectangle);
+            }
+
+        }
+        public bool CheckInRow(string VariableName)
+        {
+            foreach (DataGridViewRow item in this.Rows)
+            {
+                if(item.Cells[0].Value != null) { 
+                    if (item.Cells[0].Value.ToString().Equals(VariableName))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public int GetIndexByVarName(string VariableName)
+        {
+            for (int i = 0; i < this.Rows.Count; i++)
+            {
+                DataGridViewRow element = this.Rows[i];
+                if(element.Cells[0].Value != null)
+                {
+                    if (element.Cells[0].Value.ToString().Equals(VariableName))
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        private string tmpv="";
+        private int tmpin;
+        public void SetVariable(string VariableName,int value)
+        {
+            if (tmpv.Equals(VariableName))
+            {
+                this.Rows[tmpin].Cells[1].Value = value;
+                this.Rows[tmpin].Cells[2].Value = String.Format("0x{0}", value.ToString("X8"));
+                return;
+            }
+            tmpv = VariableName;
+            if (CheckInRow(VariableName)) {
+                int i = GetIndexByVarName(VariableName);
+                tmpin = i;
+
+                if (i >= 0)
+                {
+                    this.Rows[i].Cells[1].Value = value;
+                    this.Rows[i].Cells[2].Value = String.Format("0x{0}", value.ToString("X8"));
+                }
+            }
+            else
+            {
+                DataGridViewRow row = (DataGridViewRow)this.Rows[0].Clone();
+                row.Tag = VariableName;
+                row.Cells[0].Value = VariableName;
+                row.Cells[1].Value = value;
+                row.Cells[2].Value = String.Format("0x{0}", value.ToString("X8"));
+                row.Cells[3].Value = "int";
+                row.Height = 30;
+                tmpin = this.Rows.Add(row);
+                
+            }
             //ListViewItem item = new ListViewItem(VariableName);
             //item.SubItems.Add(VariableName);
             //item.SubItems.Add(String.Format("0x{0}", value.ToString("X8")));
